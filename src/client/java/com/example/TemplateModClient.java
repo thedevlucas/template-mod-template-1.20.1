f636bdf17074e9659b965327668e666177aa5e82
Lucas Juarez <126828696+thedevlucas@users.noreply.github.com>
2024-08-28 15:42:40 -0300

    Add files via upload

--- src/client/java/com/example/TemplateModClient.java
+++ src/client/java/com/example/TemplateModClient.java
@@ -2,28 +2,59 @@ package com.example;
 
 import com.example.dialogue.DeathWindow;
 import com.example.dialogue.DialogueManager;
+import com.example.dialogue.DialogueWindow;
 import com.example.entity.EntityTypeModule;
+import com.example.entity.ObjectivePingEntityRenderer;
 import com.example.item.ModItems;
 import com.example.packets.ModClientPackets;
 import com.example.packets.ModPackets;
-import com.example.particles.ObjectivePingEntityRenderer;
+import com.example.packets.ParticlePacket;
+import com.example.particles.DefaultParticleBuilderFactory;
+import com.example.particles.ParticleBuilderFactory;
 import com.example.particles.ParticleRegister;
+import com.example.shaders.post.BlackHoleRenderer;
 import com.example.sound.ModSounds;
+import io.netty.buffer.Unpooled;
+import ladysnake.satin.api.event.ShaderEffectRenderCallback;
+import ladysnake.satin.api.managed.ManagedShaderEffect;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderPhase;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
+
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import team.lodestar.lodestone.handlers.RenderHandler;
+import team.lodestar.lodestone.registry.client.LodestoneRenderTypeRegistry;
+import team.lodestar.lodestone.systems.particle.screen.ScreenParticleHolder;
+
+import java.awt.*;
+import java.util.Objects;
+
+
+import static com.example.packets.ModPackets.PARTICLE_SPAWN_ID;
+import ladysnake.satin.api.managed.ShaderEffectManager;
+import team.lodestar.lodestone.systems.rendering.VFXBuilders;
+import team.lodestar.lodestone.systems.rendering.rendeertype.RenderTypeToken;
 
 public class TemplateModClient implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("template-mod");
@@ -37,9 +68,10 @@ public class TemplateModClient implements ClientModInitializer {
 
 		ParticleRegister.registerFactories();
 		ParticleRegister.init();
+
 		EntityRendererRegistry.register(EntityTypeModule.OBJECTIVE_PING, ObjectivePingEntityRenderer::new);
 
-		//BlackHoleRenderer.init();
+		BlackHoleRenderer.init();
 		ModItems.registerModItems();
 		HudRenderCallback.EVENT.register(DialogueManager::renderAll);
 
